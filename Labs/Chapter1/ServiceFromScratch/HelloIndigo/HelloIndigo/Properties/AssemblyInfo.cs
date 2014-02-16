2b5d07044a59263986a541ef3688857e30388095
kietfriends <kietfriends@gmail.com>
2014-02-16 22:30:57 +0700

    1.1

--- Labs/Chapter1/ServiceFromScratch/HelloIndigo/HelloIndigo/Properties/AssemblyInfo.cs
+++ Labs/Chapter1/ServiceFromScratch/HelloIndigo/HelloIndigo/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5142014e-a473-4dd1-8ff5-136003bd77ba")]
+[assembly: Guid("3d84557f-49be-46ef-bf32-cf6887cbfcdc")]
 
 // Version information for an assembly consists of the following four values:
 //
